8b287c496bbbc142de3e74db449bb85910cebf96
Muhamad Diva Bhakti Irawan <muhammaddippa@gmail.com>
2021-06-09 15:58:33 +0700

    Menampilkan data berdasarkan ID mahasiswa

--- controller.js
+++ controller.js
@@ -3,17 +3,30 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok("Aplikasi REST API berjalan!", res);
 };
 
 //menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa = function(req, res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
-        if(error) {
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+        if (error) {
             connection.log(error);
         } else {
             response.ok(rows, res);
         }
     })
+}
+
+//menampilkan semua data mahasiswa berdasarkan IDnya
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa where id_mahasiswa = ?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                connection.log(error);
+            } else {
+                response.ok(rows, res)
+            }
+        })
 }
--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 'use strict';
 
-module.exports = function(app) {
+module.exports = function (app) {
     var jsonku = require('./controller');
 
     app.route('/')
-    .get(jsonku.index);
+        .get(jsonku.index);
 
     app.route('/tampil')
-    .get(jsonku.tampilsemuamahasiswa)
+        .get(jsonku.tampilsemuamahasiswa)
+
+    app.route('/tampil/:id')
+        .get(jsonku.tampilberdasarkanid)
 };
