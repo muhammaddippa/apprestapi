058f6d31c401a8fde48056a07d5beaf15c74c4c0
Muhamad Diva Bhakti Irawan <muhammaddippa@gmail.com>
2021-06-11 00:38:48 +0700

    Logic registerasi sukses

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,43 +1,43 @@
 var connection = require('../koneksi')
 var mysql = require('mysql')
-var bcrypt = require('bcrypt')
 var response = require('../res')
 var jwt = require('jsonwebtoken')
 var config = require('../config/secret')
 var ip = require('ip')
+var hash = require('md5')
 
 //controller untuk register
 exports.registerasi = function(req, res) {
     var post = {
         username: req.body.username,
         email: req.body.email,
-        password: bcrypt(req.body.password),
+        password: hash(req.body.password),
         role: req.body.role,
         tanggal_daftar: new Date()
     }
 
-    var query = "SELECT email FROM ?? WHERE ??"
+    var query = "SELECT email FROM ?? WHERE ??=?"
     var table = ["user", "email", post.email]
 
     query = mysql.format(query,table)
 
     connection.query(query, function(err, rows) {
-        if(error) {
-            console.log(error)
+        if(err) {
+            console.log(err)
         } else {
             if(rows.length == 0) {
                 var query = "INSERT INTO ?? SET ?"
                 var table = ["user"]
                 query = mysql.format(query, table)
                 connection.query(query, post, function(err, rows){
-                    if(error) {
-                        console.log(error)
+                    if(err) {
+                        console.log(err)
                     } else {
                         response.ok("Berhasil menambahkan data user baru!", res)
                     }
                 })
             } else {
-                response.ok("Email sudah terdaftar!")
+                response.ok("Email sudah terdaftar!", res)
             }
         }
     })
--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
 const app = express();
 
 //parse aplikasi json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 //panggil routes
 var routes = require('./routes');
 routes(app);
+
+//daftarkan menu routes dari index
+app.use('/auth', require('./middleware'))
 
 app.listen(4099, () => {
     console.log(`Server started`);
